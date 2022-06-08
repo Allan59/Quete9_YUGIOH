1aec68d385d6f8426e68af59b81c84f7e90aae42
Allan59 <allanryckelynckpro@gmail.com>
2022-06-08 10:56:12 +0200

    fff

---
+++ form.php
@@ -0,0 +1,24 @@
+<div class="container-fluid">
+    <div class="row col-12 d-flex justify-content-center ">
+        <form method="POST" action="" >
+        <div class="form-group">
+            <label for="exampleInputEmail1">Nom</label>
+            <input type="text" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Marque</label>
+            <input type="text" class="form-control" >
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Moteur</label>
+            <input type="text" class="form-control" >
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Annee</label>
+            <input type="text" class="form-control">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+    </div>
+    
+</div>
